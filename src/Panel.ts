d1c479fd5444b12803a0d471a06b258aa426cd65
Pranav Bisaria <pranavbisariya29@gmail.com>
2023-07-22 21:16:41 +0530

    Added: support for svelte

---
+++ src/Panel.ts
@@ -0,0 +1,134 @@
+import * as vscode from "vscode";
+import { getNonce } from "./Nonce";
+
+export class Panel {
+  /**
+   * Track the currently panel. Only allow a single panel to exist at a time.
+   */
+  public static currentPanel: Panel | undefined;
+
+  public static readonly viewType = "Panel";
+
+  private readonly _panel: vscode.WebviewPanel;
+  private readonly _extensionUri: vscode.Uri;
+  private _disposables: vscode.Disposable[] = [];
+
+  public static createOrShow(extensionUri: vscode.Uri) {
+    const column = vscode.window.activeTextEditor
+      ? vscode.window.activeTextEditor.viewColumn
+      : undefined;
+
+    // If we already have a panel, show it.
+    if (Panel.currentPanel) {
+      Panel.currentPanel._panel.reveal(column);
+      Panel.currentPanel._update();
+      return;
+    }
+
+    const panel = vscode.window.createWebviewPanel(
+      Panel.viewType,
+      "TrainingMug",
+      column || vscode.ViewColumn.One,
+      {
+        enableScripts: true,
+
+        localResourceRoots: [
+          vscode.Uri.joinPath(extensionUri, "media"),
+          vscode.Uri.joinPath(extensionUri, "out/compiled"),
+        ],
+      }
+    );
+
+    Panel.currentPanel = new Panel(panel, extensionUri);
+  }
+
+  public static kill() {
+    Panel.currentPanel?.dispose();
+    Panel.currentPanel = undefined;
+  }
+
+  public static revive(panel: vscode.WebviewPanel, extensionUri: vscode.Uri) {
+    Panel.currentPanel = new Panel(panel, extensionUri);
+  }
+
+  private constructor(panel: vscode.WebviewPanel, extensionUri: vscode.Uri) {
+    this._panel = panel;
+    this._extensionUri = extensionUri;
+    this._update();
+    this._panel.onDidDispose(() => this.dispose(), null, this._disposables);
+  }
+
+  public dispose() {
+    Panel.currentPanel = undefined;
+
+    // Clean up our resources
+    this._panel.dispose();
+
+    while (this._disposables.length) {
+      const x = this._disposables.pop();
+      if (x) {
+        x.dispose();
+      }
+    }
+  }
+
+  private async _update() {
+    const webview = this._panel.webview;
+
+    this._panel.webview.html = this._getHtmlForWebview(webview);
+    webview.onDidReceiveMessage(async (data) => {
+      switch (data.type) {
+        case "onInfo": {
+          if (!data.value) {
+            return;
+          }
+          vscode.window.showInformationMessage(data.value);
+          break;
+        }
+        case "onError": {
+          if (!data.value) {
+            return;
+          }
+          vscode.window.showErrorMessage(data.value);
+          break;
+        }
+        // case "tokens": {
+        //   await Util.globalState.update(accessTokenKey, data.accessToken);
+        //   await Util.globalState.update(refreshTokenKey, data.refreshToken);
+        //   break;
+        // }
+      }
+    });
+  }
+
+  private _getHtmlForWebview(webview: vscode.Webview) {
+    const scriptUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "out/compiled", "Main.js")
+    );
+
+    const stylesResetUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "media", "reset.css")
+    );
+    const stylesMainUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "media", "vscode.css")
+    );
+
+    const nonce = getNonce();
+
+    return `<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+        <meta http-equiv="Content-Security-Policy" content="img-src https: data:; style-src 'unsafe-inline' ${webview.cspSource}; script-src 'nonce-${nonce}';">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="${stylesResetUri}" rel="stylesheet">
+        <link href="${stylesMainUri}" rel="stylesheet">
+        <script nonce="${nonce}">
+        </script>
+			</head>
+      <body>
+			</body>
+      <script src="${scriptUri}" nonce="${nonce}">
+			</html>`;
+  }
+}
