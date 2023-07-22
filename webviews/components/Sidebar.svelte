52e700307d8d892006b11bf83d6592e712dcae3e
Pranav Bisaria <pranavbisariya29@gmail.com>
2023-07-23 02:04:17 +0530

    Fixed: CSS support

---
+++ webviews/components/Sidebar.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    let todos = [];
+    let count  = 0;
+</script>
+
+<style>
+    div {
+        color: brown;
+    }
+</style>
+
+<div>Hello world, I am Pranav Bisaria</div>
+<div>{count}</div>
+<button on:click={() => {
+    count++;
+}}
+>
+increment</button>
